2483ec92a9070b185069c620271859b6b083a82a
Rachel Yong <73299855+rachelyongies@users.noreply.github.com>
2022-04-02 16:52:41 +0800

    Cart microservice

--- Microservices/Cart/cart.py
+++ Microservices/Cart/cart.py
@@ -23,7 +23,7 @@ class Cart(db.Model):
     user_id = db.Column(db.String(15), primary_key=True, nullable=False)
     item_id = db.Column(db.String(15), primary_key=True)
     user_qty = db.Column(db.Integer, nullable=False)
-    item_price = db.Column(db.Float(precision=2), nullable=False)
+    
 
 
     def __init__(self, item_id, category, item_name, item_qty, item_desc, item_price, status):
@@ -59,6 +59,39 @@ class Image(db.Model):
 
 
 db.create_all()
+
+# add to cart
+@app.route("/cart/all", methods=['POST'])
+def addToCart():
+
+    data = request.get_json() #get item added
+    user_id = data["user_id"] #get user id
+    item_list = data['items'] #get item with id and qty
+
+    for item in item_list:
+        item_id = item['item_id'] 
+        qty = item['quantity']
+
+    cart = Cart(user_id, item_id, qty, datetime.now())
+    
+    try:
+        db.session.add(cart)
+        db.session.commit()
+        
+    except:
+        name = 'Add to Cart Error'
+        message = f"Error when adding to cart, for item with item_id={item_id}."
+
+        return jsonify(
+             {
+                "code": 500,
+                "data": {
+                    "item_id":item_id
+                },
+                "message": "An error occurred while adding this item to cart."
+                    
+            }
+        ),500
 
 
 # get ALL items 
@@ -96,271 +129,7 @@ def get_all():
     ), 500
     #3end
 
-####### stopped here
 
-# get all items with selected status (w 1 image URL)
-@app.route("/cart/all/<string:status>")
-def get_all_with_status(status):
-    cartlist = cart.query.filter_by(
-        status=status).all()  # gets item with status
-    output = []
-
-    for item in itemlist:  # item is an object
-        item_image = Image.query.filter_by(
-            item_id=item.item_id).first()  # object row
-
-        if item_image is None:
-            img_url = None
-        else:
-            img_url = item_image.image_url
-
-        item_dict = item.json()
-        item_dict['image_url'] = img_url
-        output.append(item_dict)
-
-    if len(output):
-        return jsonify(
-            {
-                "code": 200,
-                "message": "OK",
-                "items": output
-
-            }
-        ), 200
-    return jsonify(
-        {
-            "code": 404,
-            "message": "Item does not exist."
-        }
-    ), 404
-
-# get specific item with item_id (w all the image URLs)
-@app.route("/item/<string:item_id>")
-def get_item(item_id):
-
-    # get one item details from Item
-    # object
-    itemDetails = Item.query.filter_by(item_id=item_id).first()
-
-    # get all item images from Image
-    item_images = Image.query.filter_by(item_id=item_id).all()
-
-    itemDetails = itemDetails.json()
-
-    img_list = []
-    # for each image in images
-    for img in item_images:
-        img_list.append(img.image_url)
-
-    itemDetails['images'] = img_list
-
-    if len(itemDetails):
-        return jsonify(
-            {
-                "code": 200,
-                "message": "OK",
-                "item_details": itemDetails
-
-            }
-        ), 200
-
-    return jsonify(
-        {
-            "code": 404,
-            "message": "Item does not exist."
-        }
-    ), 404
-
-# set pending status
-@app.route("/item/setpendingstatus/<string:item_id>", methods=['PUT'])
-def setpendingstatus(item_id):
-    row = Item.query.filter_by(item_id=item_id).first()
-    if (row.status == 'pending'):
-        row.status = 'shipping'
-        try:
-            db.session.commit()
-        except:
-            name = 'Item Status Change'
-            message = f"Error when changing item status from 'Pending' to 'Shipping', for item with item_id={item_id}."
-
-            return jsonify(
-                {
-                    "code": 500,
-                    "message": 'Error occurred while changing status.'
-                }
-            ), 500
-
-    name = 'Item Status Change'
-    message = f"The item status for item with item_id={item_id}, has just been changed from 'Pending' to 'Shipping'."
-
-    return jsonify(
-        {
-            "code": 200,
-            "message": 'OK.',
-            "item_name": row.item_name
-        }
-    ), 200
-
-# set shipping status
-@app.route("/item/setshippingstatus/<string:item_id>", methods=['PUT'])
-def setshippingstatus(item_id):
-    row = Item.query.filter_by(item_id=item_id).first()
-    if (row.status == 'shipping'):
-        row.status = 'complete'
-        try:
-            db.session.commit()
-        except:
-            name = 'Item Status Change'
-            message = f"Error when changing item status from 'Shipping' to 'Complete', for item with item_id={item_id}."
-
-            return jsonify(
-                {
-                    "code": 500,
-                    "message": 'Error occurred while changing status.'
-                }
-            ), 500
-
-    name = 'Item Status Change'
-    message = f"The item status for item with item_id={item_id}, has just been changed from 'Shipping' to 'Complete'."
-
-    return jsonify(
-        {
-            "code": 200,
-            "message": 'OK.'
-        }
-    ), 200
-
-# set archive status when item is pending
-@app.route("/item/setarchivestatus/<string:item_id>", methods=['PUT'])
-def setarchivestatus(item_id):
-    row = Item.query.filter_by(item_id=item_id).first()
-
-    if row is None:
-        return jsonify(
-            {
-                "code": 400,
-                "message": "Item not found."
-            }
-        )
-
-    if (row.status == 'pending'):
-        row.status = 'archived'
-        try:
-            db.session.commit()
-        except:
-            name = 'Item Status Change'
-            message = f"Error when changing item status from 'Pending' to 'Archived', for item with item_id={item_id}."
-
-            return jsonify(
-                {
-                    "code": 500,
-                    "message": 'Error occurred while changing status.'
-                }
-            ), 500
-
-        name = 'Item Status Change'
-        message = f"The item status for item with item_id={item_id}, has just been changed from 'Pending' to 'Archived'."
-
-        return jsonify(
-            {
-                "code": 200,
-                "message": 'OK.',
-                "item_name": row.item_name
-            }
-        ), 200
-    else:
-        name = 'Item Status Change'
-        message = f"Error when archiving item as status is not pending, for item with item_id={item_id}."
-
-        return jsonify(
-            {
-                "code": 400,
-                "message": "Unable to archive item as item status is not pending."
-            }
-        ), 400
-
-
-# find item by category
-@app.route("/item/category/<string:category>")
-def find_by_category(category):
-    itemList = Item.query.filter_by(category=category).all()
-    if len(itemList):
-        return jsonify(
-            {
-                "code": 200,
-                "message": 'OK.',
-                "data": [item.json() for item in itemList]
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "message":  "Item category does not exist."
-        }
-    ), 404
-
-
-# add to 2 tables image and item
-@app.route("/item", methods=['POST'])
-def add_item():
-    data = request.get_json()
-    item_id = ''.join(
-        [random.choice(string.ascii_letters + string.digits) for n in range(15)])
-
-    category = data["category"]
-    item_name = data["item_name"]
-    item_qty = data["item_qty"]
-    item_desc = data["item_desc"]
-    item_price = data["item_price"]
-    status = 'pending'
-    image_url = data["image_url"]
-
-    item = Item(item_id, category, item_name, item_qty, item_desc,
-                item_price, status)
-    item_json = item.json()
-
-    try:
-        db.session.add(item)
-        db.session.commit()
-
-    except:
-        name = 'Item Creation'
-        message = f"Error when adding item, for item with item_id={item_id}."
-
-        return jsonify(
-            {
-                "code": 500,
-                "message": 'Error occurred while adding item.'
-            }
-        ), 500
-
-    for url in image_url:
-        item_image = Image(item_id, url)
-        try:
-            db.session.add(item_image)
-            db.session.commit()
-
-        except:
-            name = 'Item Creation'
-            message = f"Error when adding item_image, for item with item_id={item_id}."
-
-            return jsonify(
-                {
-                    "code": 500,
-                    "message": 'Error occurred while adding item_image.'
-                }
-            ), 500
-
-
-    name = 'Item Creation'
-    message = f"The item with item_id={item_id}, has been successfully added into the db."
-
-    return jsonify(
-        {
-            "code": 201,
-            "message": 'Item successfully created.'
-        }
-    ), 201
 
 
 if __name__ == '__main__':
